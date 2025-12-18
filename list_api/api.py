ace42fcfe6e242b0e6f333fc064c7c0bf4191ea8
ScavazzaRian <rianscavazza.git@gmail.com>
2025-12-18 11:15:59 -0300

    feat: ajusta API para retornar as respostas corretas

--- list_api/api.py
+++ list_api/api.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -13,7 +13,7 @@ class Produto(BaseModel):
 data = []
 id_inicial = 1
 
-@app.post("/create", response_model=Produto)
+@app.post("/create", response_model=Produto, status_code=status.HTTP_201_CREATED, tags=["Produtos"])
 def create_product(produto: Produto):
     global id_inicial
     produto.id = id_inicial
@@ -23,23 +23,32 @@ def create_product(produto: Produto):
     id_inicial += 1
     return produto
 
-@app.get("/itens", response_model=list[Produto])
+@app.get("/itens", response_model=list[Produto], status_code=status.HTTP_200_OK, tags=["Produtos"])
 def read_products():
     return data
 
-@app.delete("/itens/delete/{id}", response_model=Produto)
+@app.delete("/itens/delete/{id}", response_model=Produto, status_code=status.HTTP_200_OK, tags=["Produtos"])
 def delete_product(id: int):
     for produto in data:
         if produto.id == id:
             data.remove(produto)
             return produto
+    
+    raise HTTPException(status_code=404, detail="Produto Inexistente")
         
-@app.put("/itens/update/{id}", response_model=Produto)
+@app.put("/itens/update/{id}", response_model=Produto, status_code=status.HTTP_200_OK, tags=["Produtos"])
 def update_product(id: int, produto_atualizado: Produto):
     for produto in data:
         if produto.id == id:
-            produto.name = produto_atualizado.name
-            produto.price = produto_atualizado.price
-            produto.descricao = produto_atualizado.descricao
+            if not produto_atualizado.name or not produto_atualizado.descricao:
+                raise HTTPException(status_code=400, detail="Nome e descricao nao podem ser vazios")
+            elif produto_atualizado.price <= 0:
+                raise HTTPException(status_code=400, detail="O preco do produto deve ser maior que 0")
+            else:
+                produto.name = produto_atualizado.name
+                produto.price = produto_atualizado.price
+                produto.descricao = produto_atualizado.descricao
 
             return produto
+        
+    raise HTTPException(status_code=404, detail="Produto Inexistente")
