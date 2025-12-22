8bb27330a8b3cadaaaf519587887c11d288bb2b0
ScavazzaRian <rianscavazza.git@gmail.com>
2025-12-22 11:58:14 -0300

    feat: finalizando CRUD com o FastAPI + SQLAlchemy

--- orm_api/app/main.py
+++ orm_api/app/main.py
@@ -1,13 +1,14 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app.database import engine, get_db
 from app import models, schemas
+from typing import List
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-@app.post("/produtos/create", response_model=schemas.ProdutoResponse)
+@app.post("/produtos/create", response_model=schemas.ProdutoResponse, status_code=201)
 def create(request: schemas.ProdutoCreate, db: Session = Depends(get_db)):
     produto = models.Produto(**request.model_dump())
 
@@ -16,3 +17,36 @@ def create(request: schemas.ProdutoCreate, db: Session = Depends(get_db)):
     db.refresh(produto)
 
     return produto
+
+@app.get("/produtos", response_model=List[schemas.ProdutoResponse], status_code=200)
+def read(db:Session = Depends(get_db)):
+    produtos = db.query(models.Produto).all()
+    return produtos
+
+@app.delete("/produtos/{id}/delete", status_code=204)
+def delete(id: int, db: Session = Depends(get_db)):
+    query = db.query(models.Produto).filter(models.Produto.id == id)
+    produto = query.first()
+
+    if not produto:
+        raise HTTPException(status_code=404, detail="Produto nao encontrado")
+    
+    query.delete(synchronize_session=False)
+    db.commit()
+
+    return None
+
+@app.patch("/produtos/{id}/update", response_model=schemas.ProdutoResponse, status_code=201)
+def update(id: int, request: schemas.ProdutoCreate, db: Session = Depends(get_db)):
+    query = db.query(models.Produto).filter(models.Produto.id == id)
+    produto = query.first()
+
+    if not produto:
+        raise HTTPException(status_code=404, detail="Produto nao encontrado")
+
+    dados_atualizados = request.model_dump(exclude_unset=True)
+
+    query.update(dados_atualizados)
+    db.commit()
+    
+    return query.first() 
